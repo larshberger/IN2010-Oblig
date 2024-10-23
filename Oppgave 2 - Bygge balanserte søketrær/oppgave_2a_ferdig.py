a30806dd25c989122372308f2361e95673d6338c
larshberger <larshusbyberger@gmail.com>
2024-10-23 12:43:02 +0200

    2a ferdig

---
+++ Oppgave 2 - Bygge balanserte søketrær/oppgave_2a_ferdig.py
@@ -0,0 +1,15 @@
+def print_array(arr, start, slutt):
+    if start > slutt:
+        return 
+    
+    mid = (start + slutt) // 2 
+    print(arr[mid]) 
+    
+    print_array(arr, start, mid - 1) 
+    print_array(arr, mid + 1, slutt) 
+
+if __name__ == "__main__":
+    import sys
+    input_numbers = list(map(int, sys.stdin.read().strip().split()))
+    print_array(input_numbers, 0, len(input_numbers) - 1)
+
